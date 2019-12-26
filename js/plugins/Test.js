cdcc7a711a8bd04fc71c766b896129f88bcd6f96
GeorgeChen-666 <36164378+GeorgeChen-666@users.noreply.github.com>
2019-12-26 16:50:34 +0800

    .

--- js/plugins/Test.js
+++ js/plugins/Test.js
@@ -12,14 +12,15 @@
     }
     return len;
   };
-  class Window_Text extends Window_Base {
+  class Window_TextBalloon extends Window_Base {
     constructor() {
       super(...arguments);
     }
-    initialize(text = '', x = 0, y = 0) {
-      this._text = text;
-      Window_Base.prototype.initialize.call(this, x, y,140,60);
+    initialize() {
+      this._text = '';
+      Window_Base.prototype.initialize.call(this, 0, 0,140,60);
       this.move(0,0,0,0);
+      this.close();
       this._arrowSprite = new Sprite(new Bitmap(20, 10));
       this._arrowSprite.visible = false;
       const skin = this._windowskin;
@@ -49,49 +50,42 @@
       const {x,y} = this;
       const width = this.textWidth(rtxt.split('\n')[0]) + this.standardPadding() * 2;
       const height = this.fittingHeight(rtxt.includes('\n')?2:1);
-      this.move(x,y,width,height);
-      this._arrowSprite.visible = true;
-      this._arrowSprite.x = this.width / 4;
-      this._arrowSprite.y = this.height - 1;
+      const offSetX = - 24;
+      const offSetY = -height - 52;
+      this.move(offSetX,offSetY,width,height);
+      this._arrowSprite.x = 0;
+      this._arrowSprite.y = this.height - 1 + offSetY;
+      setTimeout(()=>{
+        this._arrowSprite.visible = true;
+      },200);
 
       this.open();
-      console.log('开始'+new Date());
       setTimeout(() => {
         this.close();
         this._arrowSprite.visible = false;
         this._text = undefined;
         this.parent._character._balloonText = undefined;
-        console.log('结束'+new Date());
-      }, 2500);
+      }, 3000);
     }
     refresh() {
       this.contents.clear();
       this.drawTextEx(this._text, 0, 0);
     }
   }
-  window.Window_Text = Window_Text;
-  //Scene_Map.prototype.createAllWindows
-  // const Scene_Title_createCommandWindow = Scene_Title.prototype.createCommandWindow;
-  // Scene_Title.prototype.createCommandWindow = function() {
-  //   Scene_Title_createCommandWindow.call(this);
-  //   this._textWindow = new Window_Text();
-  //   this.addWindow(this._textWindow);
-  //   this._textWindow.setText('你好啊你好啊你好啊你好啊你好啊你好啊你好啊');
-  // };
   const Sprite_Character_initMembers = Sprite_Character.prototype.initMembers;
   Sprite_Character.prototype.initMembers = function() {
     Sprite_Character_initMembers.call(this);
-    this._textWindow = new Window_Text();
+    this._textWindow = new Window_TextBalloon();
     this.addChild(this._textWindow);
   };
   const Sprite_Character_setupBalloon = Sprite_Character.prototype.setupBalloon;
   Sprite_Character.prototype.setupBalloon = function() {
     Sprite_Character_setupBalloon.call(this);
-    if (this._character._balloonText !== undefined) {
+    if (this._character._balloonText !== undefined && this._textWindow.isClosed()) {
       this._textWindow.setText(this._character._balloonText);
     }
   };
-  Game_CharacterBase.prototype.setupTextBalloon = function(text) {
-    this._balloonText = text;
+  Game_Interpreter.prototype.setupTextBalloon = function(text) {
+    this.character()._balloonText = text;
   };
 })();
